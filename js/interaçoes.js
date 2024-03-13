9a6fb189166ee043125824e08c62c61050cd4d08
mykoll007 <mykoll.daniel@gmail.com>
2024-03-13 11:56:56 -0300

    first

---
+++ js/interaçoes.js
@@ -0,0 +1,10 @@
+//Hamburguer
+function clickMenu(){
+  
+    if(navheader.style.display == 'block'){
+        navheader.style.display = 'none'
+    }else{
+        navheader.style.display = 'block'
+    }
+
+}

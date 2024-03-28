baf9e70baa539d187b8958e711cab66fd0d3a55b
mykoll007 <mykoll.daniel@gmail.com>
2024-03-28 16:52:40 -0300

    começando JavaScript no projeto

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const imgs = document.getElementById("img-carrossel");
+const img = document.querySelectorAll("#img-carrossel img");
+
+let idx = 0;
+
+function carrossel(){
+    idx++;
+
+    if(idx > img.length - 1){
+        idx = 0;
+    }
+
+    imgs.style.transform = `translateX(${-idx * 92}%)`;
+}
+setInterval(carrossel, 1800);

e8208e8ee74be9c9e9e33c367a94b4d65660baec
mykoll007 <mykoll.daniel@gmail.com>
2024-04-01 00:53:45 -0300

    quase finalizado

--- js/script.js
+++ js/script.js
@@ -13,3 +13,44 @@ function carrossel(){
     imgs.style.transform = `translateX(${-idx * 98}%)`;
 }
 setInterval(carrossel, 1800);
+
+let timer;
+let secondsRemaining = 24 * 3600 + 59 * 60 + 59;
+
+function updateTimer() {
+  secondsRemaining--;
+  if (secondsRemaining <= 0) {
+    clearInterval(timer);
+    secondsRemaining = 24 * 3600 + 59 * 60 + 59; // Reiniciar para 24 horas, 59 minutos e 59 segundos
+  }
+  updateDisplay();
+}
+
+function updateDisplay() {
+  const hours = Math.floor(secondsRemaining / 3600);
+  const minutes = Math.floor((secondsRemaining % 3600) / 60);
+  const seconds = secondsRemaining % 60;
+  const formattedTime = pad(hours) + ":" + pad(minutes) + ":" + pad(seconds);
+  document.querySelector(".timer").innerText = formattedTime;
+}
+
+function pad(value) {
+  return value < 10 ? "0" + value : value;
+}
+
+// Iniciar o cronômetro automaticamente
+timer = setInterval(updateTimer, 1000);
+
+function openModal() {
+    document.getElementById("modal").style.display = "block";
+  }
+  
+  function closeModal() {
+    document.getElementById("modal").style.display = "none";
+  }
+  
+  window.onclick = function(event) {
+    if (event.target == document.getElementById("modal")) {
+      closeModal();
+    }
+  }
